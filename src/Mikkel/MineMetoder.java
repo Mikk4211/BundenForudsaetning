6c4586819cf9a5bb0a549ea957080e14e10b48eb
Mikkel Sørensen <mik4211@edu.easj.dk>
2017-10-24 12:32:01 +0200

    Sidste version af programmet.

--- src/Mikkel/MineMetoder.java
+++ src/Mikkel/MineMetoder.java
@@ -1,14 +1,6 @@
 package Mikkel;
-
-import java.util.Scanner;
-
 public class MineMetoder {
-    static Scanner aT = new Scanner(System.in);
-
-    // Input til hvor mange terninger (1 eller 2 terninger, preferably)
-    static int terninger = aT.nextInt();
-
-
+    // random statement.
     public static int rand(int max, int min) {
         int range = (max - min) + 1;
         int random = (int) ((Math.random() * range) + min);
